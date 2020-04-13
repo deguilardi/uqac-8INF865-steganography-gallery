1359829e6ca241b3758cde659783735d3924b1ea
Loic <loic.tang@isen.yncrea.fr>
2020-04-13 12:18:45 -0400

    Revert "it does not work"
    This reverts commit 54821eddcaa8d07a00cea6c1be1d387b1ddb6c95.

--- app/src/main/java/ca/uqac/steganographygallery/activities/MainActivity.java
+++ app/src/main/java/ca/uqac/steganographygallery/activities/MainActivity.java
@@ -1,24 +1,17 @@
 package ca.uqac.steganographygallery.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.Display;
 import android.view.Surface;
 import android.view.WindowManager;
-import android.widget.Toast;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -29,7 +22,6 @@ import ca.uqac.steganographygallery.R;
 
 public class MainActivity extends AppCompatActivity implements PicturesAdapter.PicturesAdapterOnClickHandler {
 
-
     private PicturesAdapter mPicturesAdapter;
 
     @BindView(R.id.recyclerview_pic_list) RecyclerView mRecyclerView;
@@ -38,22 +30,9 @@ public class MainActivity extends AppCompatActivity implements PicturesAdapter.P
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //pour la permission de stockage
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
-        {
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1000);
-
-        }
-        else
-            {
-            // affiche l'image
-                ButterKnife.bind(this);
-                setupUI();
-                loadPictures();
-            }
-
-
-
+        ButterKnife.bind(this);
+        setupUI();
+        loadPictures();
     }
 
     private void setupUI(){
@@ -85,17 +64,7 @@ public class MainActivity extends AppCompatActivity implements PicturesAdapter.P
         picturesList.add(getURLForResource(R.drawable.test6));
         picturesList.add(getURLForResource(R.drawable.test7));
         picturesList.add(getURLForResource(R.drawable.test8));
-
-        String path = Environment.getExternalStorageDirectory().toString()+"/Images-";
-        File directory = new File(path);
-        File[] files = directory.listFiles();
-        for (int i = 0; i < files.length; i++){
-            picturesList.add(path);
-        }
-
         mPicturesAdapter.swapData(picturesList);
-
-
     }
 
     public String getURLForResource (int resourceId) {
@@ -111,29 +80,5 @@ public class MainActivity extends AppCompatActivity implements PicturesAdapter.P
         Intent detailsActivityIntent = new Intent(MainActivity.this, DetailActivity.class);
         detailsActivityIntent.putExtra(DetailActivity.PARAM_PICTURE, filePath);
         startActivity(detailsActivityIntent, options.toBundle());
-    }
-
-
-   @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-   {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode==1000)
-        {
-            if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                //show the image
-                ButterKnife.bind(this);
-                setupUI();
-                loadPictures();
-
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
-
-            }
-            else{
-                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
-                finish();
-
-            }
-        }
     }
 }
